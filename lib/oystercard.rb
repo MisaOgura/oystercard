f6841fd0acde48c2bc9d46910c86aef010e4bcde
Murilo Dal Ri <nope@noway.no>
2016-03-07 15:52:06 +0000

    Added a default balance and check balance method.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,14 @@
 class Oystercard
+  DEFAULT_BALANCE = 5
+
+  def initialize(balance=DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def check_balance
+    balance
+  end
+
+  private
+  attr_reader :balance
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-
+subject(:card) { described_class.new }
+  it 'has a balance' do
+    expect(subject.check_balance).to eq Oystercard::DEFAULT_BALANCE
+  end
 end
