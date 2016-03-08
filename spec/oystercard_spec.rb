20130f2e8f709eeb0ffbfaecf4633d5b72d3347a
Misa Ogura <misa.ogura01@gmail.com>
2016-03-08 06:53:56 +0000

    Substitute subject with card in unit tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,27 +13,27 @@ single_fare = Oystercard::SINGLE_FARE
 
   describe '#initialize' do
     it 'creates a card with empty history' do
-      expect(subject.check_history.empty?).to eq true
+      expect(card.check_history.empty?).to eq true
     end
   end
 
   describe '#check_balance' do
     it 'has a balance' do
-      expect(subject.check_balance).to eq default_balance
+      expect(card.check_balance).to eq default_balance
     end
   end
 
   describe '#top_up' do
     it 'adds money to the balance' do
       amount = Random.rand(5..10)
-      expect(subject.top_up(amount)).to eq (default_balance + amount)
+      expect(card.top_up(amount)).to eq (default_balance + amount)
     end
 
     context 'when maximum balance exceeded' do
       it 'raises an error' do
         amount = Random.rand(100..200)
         message = "maximum balance is £#{maximum}"
-        expect{subject.top_up(amount)}.to raise_error message
+        expect{card.top_up(amount)}.to raise_error message
       end
     end
   end
@@ -49,32 +49,32 @@ single_fare = Oystercard::SINGLE_FARE
     context 'when the balance is more than minimum' do
 
       before(:each) do
-        subject.touch_in entry_station
+        card.touch_in entry_station
       end
 
       it 'changes in_journey? to true' do
-        expect(subject.in_journey?).to eq true
+        expect(card.in_journey?).to eq true
       end
     end
   end
 
   describe '#touch_out' do
     it 'changes in_journey? to false' do
-      subject.touch_out exit_station
-      expect(subject.in_journey?).to eq false
+      card.touch_out exit_station
+      expect(card.in_journey?).to eq false
     end
 
     it 'deducts a fare from the card' do
-      expect{subject.touch_out(exit_station)}.to change{subject.check_balance}.by(-single_fare)
+      expect{card.touch_out(exit_station)}.to change{card.check_balance}.by(-single_fare)
     end
   end
 
   describe '#check_history' do
 
     it 'shows the history of one journey' do
-      subject.touch_in entry_station
-      subject.touch_out exit_station
-      expect(subject.check_history[entry_station]).to eq exit_station
+      card.touch_in entry_station
+      card.touch_out exit_station
+      expect(card.check_history[entry_station]).to eq exit_station
     end
   end
 end
