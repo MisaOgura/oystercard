0fc6ef0eafc77782d0933b8e4f6735ddc87d694d
Misa Ogura <misa.ogura01@gmail.com>
2016-03-08 07:22:39 +0000

    Modify initialize method to create an object with a correct zone for the station given, using hash within the method

--- lib/station.rb
+++ lib/station.rb
@@ -1,8 +1,13 @@
 class Station
   attr_reader :zone, :name
 
-  def initialize
-    @name = ''
-    @zone = 0
+  def initialize (name)
+    list = {holborn: 1, algate: 1,
+            turnpike_lane: 3,
+            arsenal: 2, wimbledon: 3,
+            richmond: 4, kew_garden: 4}
+
+    @name = name.downcase.gsub(' ', '_').to_sym
+    @zone = list[@name]
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,16 +2,14 @@ require 'station'
 
 describe Station do
 
-subject(:station) { described_class.new }
+subject(:station) { described_class.new('Turnpike Lane') }
 
   describe '#initialize' do
     it 'creates a station with name' do
-      expect(station.name).not_to be eq nil
+      expect(station.name).not_to eq nil
     end
     it 'creates a station with zone attribute' do
-      expect(station.zone).not_to be eq nil
+      expect(station.zone).to eq 3
     end
-
-
   end
 end
